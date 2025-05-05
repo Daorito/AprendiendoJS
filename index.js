946096ff1ec4b23427bcef60ea9a8bad99b4576f
Daoritox <davidortega0099@gmail.com>
2025-05-05 20:56:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let cities = [ "New York", "Los Angeles", "Chicago", "Houston", "Phoenix" ];
+console.log(cities)
+
+
+cities[ cities.length - 1 ] = "Philadelphia"; // cambia el último elemento por "Philadelphia". 
+console.log(cities)
+
