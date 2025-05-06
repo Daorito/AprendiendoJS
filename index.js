fb857b281017fe236e76e3c7f184d47e0728f8a5
Daoritox <davidortega0099@gmail.com>
2025-05-06 20:54:03 +0200

    Ejemplo de blucle 'for' basico.

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ console.log(cities)
 cities[ cities.length - 1 ] = "Philadelphia"; // cambia el último elemento por "Philadelphia". 
 console.log(cities)
 
+console.log("Ejemplo de bucle for clásico:");
+for (let i = 0; i < 5; i++) {
+    console.log("El valor de i es: " + i);
+}
+// Salida:
+// El valor de i es: 0
+// El valor de i es: 1
+// El valor de i es: 2
+// El valor de i es: 3
+// El valor de i es: 4
